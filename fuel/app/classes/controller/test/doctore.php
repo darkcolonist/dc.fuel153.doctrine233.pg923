0194c09ef80da5d9a3a32df2b993c9ee4f747b20
Christian Noel Reyes <darkcolonist@gmail.com>
2013-04-22 18:29:10 +0800

    doctrine entities not yet working - i tell you, its HARD!

--- fuel/app/classes/controller/test/doctore.php
+++ fuel/app/classes/controller/test/doctore.php
@@ -2,7 +2,7 @@
 class Controller_Test_Doctore extends Controller_Test
 {
   public function action_index(){
-    $_t = "OH MY LORD <blink>IT WORKS!!!</blink>";
+    $_t = "OH MY GOODNESS <blink>IT WORKS!!!</blink>";
 
     $results = Doctrine::$em->getConnection()
             ->fetchAll("SELECT * FROM users;");
@@ -13,5 +13,51 @@ class Controller_Test_Doctore extends Controller_Test
 
     return Response::forge($_t);
 
+  }
+
+  public function action_generate(){
+    $_t = "OH MY GOODNESS <blink>IT WORKS!!!</blink>";
+    $_t .= "<br />setting up nao, wait plox!";
+
+    // fetch metadata
+    $driver = new \Doctrine\ORM\Mapping\Driver\DatabaseDriver(
+        Doctrine::$em->getConnection()->getSchemaManager()
+    );
+    Doctrine::$em->getConfiguration()->setMetadataDriverImpl($driver);
+    $cmf = new \Doctrine\ORM\Tools\DisconnectedClassMetadataFactory(Doctrine::$em);
+    $cmf->setEntityManager(Doctrine::$em);
+    $classes = $driver->getAllClassNames();
+    $metadata = $cmf->getAllMetadata();
+    $generator = new \Doctrine\ORM\Tools\EntityGenerator();
+    $generator->setUpdateEntityIfExists(true);
+    $generator->setGenerateStubMethods(true);
+    $generator->setGenerateAnnotations(true);
+    $generator->generate($metadata, APPPATH. 'classes/model/Entities');
+
+    $_t .= "<br />Done!";
+
+    return Fuel\Core\Response::forge($_t);
+  }
+
+  public function action_query(){
+    $user = new Users();
+    $user->setFirstName("luka");
+
+    $_t = "i'm fvcked";
+
+    $_t .= " ".$user->getFirstName();
+
+    Doctrine::$em->persist($user);
+    Doctrine::$em->flush();
+
+//    $query = Doctrine::$em->createQueryBuilder()
+//            ->select("u")
+//            ->from("Users", "u")
+//            ->getQuery();
+//    $users = $query->getResult();
+//
+//    $_t = print_r($users->toArray(), true);
+
+    return Fuel\Core\Response::forge($_t);
   }
 }
--- fuel/packages/doctrine/bootstrap.php
+++ fuel/packages/doctrine/bootstrap.php
@@ -1,10 +1,8 @@
 <?php
-/// V4
-use Doctrine\Common\ClassLoader,
-    Doctrine\ORM\Configuration,
-    Doctrine\ORM\EntityManager,
-    Doctrine\Common\Cache\ArrayCache,
-    Doctrine\DBAL\Logging\EchoSQLLogger;
+/// V5
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\ClassLoader;
 
 class Doctrine {
 
@@ -18,30 +16,9 @@ class Doctrine {
 
     $doctrineClassLoader = new ClassLoader('Doctrine',  dirname(__FILE__).'/');
     $doctrineClassLoader->register();
-    $entitiesClassLoader = new ClassLoader('model', rtrim(APPPATH, "/classes/" ));
-    $entitiesClassLoader->register();
-    $proxiesClassLoader = new ClassLoader('Proxies', APPPATH.'classes/model/proxies');
-    $proxiesClassLoader->register();
 
-    // Set up caches
-    $config = new Configuration;
-    $cache = new ArrayCache;
-    $config->setMetadataCacheImpl($cache);
-    $driverImpl = $config->newDefaultAnnotationDriver(array(APPPATH.'model/Entities'));
-    $config->setMetadataDriverImpl($driverImpl);
-    $config->setQueryCacheImpl($cache);
-
-    $config->setQueryCacheImpl($cache);
-
-    // Proxy configuration
-    $config->setProxyDir(APPPATH.'/models/proxies');
-    $config->setProxyNamespace('Proxies');
-
-    // Set up logger
-//    $logger = new EchoSQLLogger;
-//    $config->setSQLLogger($logger);
-
-    $config->setAutoGenerateProxyClasses( TRUE );
+    $isDevMode = true;
+    $config = Setup::createAnnotationMetadataConfiguration(array(APPPATH."classes/model/Entities"), $isDevMode);
 
     Config::load("db", true);
     $db = Config::get("db.default.connection");
@@ -63,6 +40,69 @@ class Doctrine {
 }
 
 Doctrine::setup();
+
+/// V4
+//use Doctrine\Common\ClassLoader,
+//    Doctrine\ORM\Configuration,
+//    Doctrine\ORM\EntityManager,
+//    Doctrine\Common\Cache\ArrayCache,
+//    Doctrine\DBAL\Logging\EchoSQLLogger;
+//
+//class Doctrine {
+//
+//  static $em = null;
+//
+//  public static function setup()
+//  {
+//    // Set up class loading. You could use different autoloaders, provided by your favorite framework,
+//    // if you want to.
+//    require_once dirname(__FILE__).'/Doctrine/Common/ClassLoader.php';
+//
+//    $doctrineClassLoader = new ClassLoader('Doctrine',  dirname(__FILE__).'/');
+//    $doctrineClassLoader->register();
+//    $entitiesClassLoader = new ClassLoader('Entities', APPPATH."classes/model/Entities" );
+//    $entitiesClassLoader->register();
+//    $proxiesClassLoader = new ClassLoader('Proxies', APPPATH.'classes/model/Proxies');
+//    $proxiesClassLoader->register();
+//
+//    // Set up caches
+//    $config = new Configuration;
+//    $cache = new ArrayCache;
+//    $config->setMetadataCacheImpl($cache);
+//    $driverImpl = $config->newDefaultAnnotationDriver(array(APPPATH.'classes/model/Entities'), false);
+//    $config->setMetadataDriverImpl($driverImpl);
+//    $config->setQueryCacheImpl($cache);
+//
+//    // Proxy configuration
+//    $config->setProxyDir(APPPATH.'classes/models/Proxies');
+//    $config->setProxyNamespace('Proxies');
+//
+//    // Set up logger
+////    $logger = new EchoSQLLogger;
+////    $config->setSQLLogger($logger);
+//
+//    $config->setAutoGenerateProxyClasses( TRUE );
+//
+//    Config::load("db", true);
+//    $db = Config::get("db.default.connection");
+//
+//    // Database connection information
+//    $connectionOptions = array(
+//        'driver'    => $db['driver'],
+//        'dsn'       => $db['dsn'],
+//        'user'      => $db['username'],
+//        'password'  => $db['password'],
+//        'host'      => $db['hostname'],
+//        'dbname'    => $db['database'],
+//        'port'      => $db['port']
+//    );
+//
+//    // Create EntityManager
+//    self::$em = EntityManager::create($connectionOptions, $config);
+//  }
+//}
+//
+//Doctrine::setup();
 
 /// V3
 //use Doctrine\Common\ClassLoader;
